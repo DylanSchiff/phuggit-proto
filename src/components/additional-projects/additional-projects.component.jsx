d05890e9cc1cb02991bf03de7c0459b95c38bef2
Dylan <schiff.dylan@gmail.com>
2022-11-13 10:11:49 -0700

    spacing

--- src/components/additional-projects/additional-projects.component.jsx
+++ src/components/additional-projects/additional-projects.component.jsx
@@ -12,14 +12,15 @@ const AdditionalProjectsContainer = styled.div`
     border-radius: 25px;
     display: ${({ display }) => display};
     grid-template-columns: repeat(4, 1fr);
-    /* flex-flow: ; */
+    grid-gap: 20px;
+    margin: 20px 0;
     align-items: center;
     justify-items: center;
     @media only screen and (max-width: 1000px) {
-        grid-template-columns: repeat(3, 1fr);
+        grid-template-columns: repeat(2, 1fr);
     }
     @media only screen and (max-width: 600px) {
-        grid-template-columns: repeat(2, 1fr);
+        grid-template-columns: repeat(1, 1fr);
     }
 `;
 const AddProjectContainer = styled.div`
@@ -27,7 +28,7 @@ const AddProjectContainer = styled.div`
     box-shadow: 0 1px 3px var(--fade-001), 0 2px 5px var(--fade-001),
         0 3px 8px var(--fade-002);
     border-radius: 17.5px;
-    margin: 40px 20px 20px 0;
+    /* margin: 40px 20px 20px 0; */
     padding: 10px;
     display: flex;
     flex-flow: column nowrap;
@@ -35,42 +36,33 @@ const AddProjectContainer = styled.div`
     justify-content: center;
     cursor: pointer;
     border: 1px solid var(--fade-001);
+    flex: 1;
+    width: 100%;
 `;
 const AddProjectImageContainer = styled.div`
-    /* max-height: 150px;
-    max-width: 150px;
-    min-height: 150px;
-    min-width: 150px;
-    border-radius: 12.5px;
-    overflow: hidden; 
-    background-color: var(--white-002);
-    box-shadow: inset 0 1px 3px var(--fade-001), inset 0 2px 5px var(--fade-001),
-        inset 0 3px 8px var(--fade-002);
-    border: 1px solid var(--fade-001);
-    */
     display: flex;
     align-items: center;
     justify-content: center;
     transition: 111ms linear;
     margin-bottom: 20px;
+    max-height: 150px;
+    /* max-width: 150px; */
+    min-height: 150px;
 
     @media only screen and (max-width: 600px) {
         max-height: 100px;
-        max-width: 100px;
+        /* max-width: 100px; */
         min-height: 100px;
-        /* min-width: 100px; */
     }
 `;
 const AddProjectImage = styled.img`
     max-height: 125px;
-    max-width: 125px;
+    /* max-width: 125px; */
     min-height: 125px;
-    /* min-width: 125px; */
     @media only screen and (max-width: 600px) {
         max-height: 80px;
-        max-width: 80px;
+        /* max-width: 80px; */
         min-height: 80px;
-        /* min-width: 80px; */
     }
 `;
 const PROJECTS = [
