d1e8814a431a72d3104eeb1cf03b926b12d98352
Dylan <schiff.dylan@gmail.com>
2022-11-16 12:14:51 -0700

    colorcube

--- src/components/colorcube/color-card.component.jsx
+++ src/components/colorcube/color-card.component.jsx
@@ -1,14 +1,90 @@
 import { useState } from "react";
 import styled from "styled-components";
 import ColorCube from "./colorcube.component";
-const ColorCardContainer = styled.div`
+const ColorCardContainer = styled.div.attrs((props) => ({
+    style: {
+        backgroundColor: props.backgroundcolor,
+        border: "1px solid " + props.bordercolor,
+    },
+}))`
     height: 300px;
     width: 300px;
-    background-color: #fff;
+    transition: 111ms linear;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+    border-radius: 20px;
 `;
-const ColorCard = () => {
-    const [currentColor, setCurrentColor] = useState("#555");
+const ClapOn = styled.div`
+    flex: 1;
+    width: 100%;
+    text-align: center;
+    padding: 20px;
+`;
+const ClapOnLabel = styled.span.attrs((props) => ({
+    style: {
+        color: props.color,
+    },
+}))`
+    font-size: 25px;
+    font-weight: 500;
+    user-select: none;
+    transition: 111ms linear;
+`;
+const ColorCubeDisplay = styled.div`
+    flex: 1;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+const ColorDetails = styled.div`
+    flex: 1;
+    width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+`;
+const DetailsDuo = styled.div``;
+const DetailsLabel = styled.span.attrs((props) => ({
+    style: {
+        color: props.color,
+    },
+}))`
+    margin-right: 5px;
+`;
+const DetailsInfo = styled.span.attrs((props) => ({
+    style: {
+        color: props.color,
+    },
+}))``;
 
+const ColorCard = () => {
+    const [currentColor, setCurrentColor] = useState("#555555");
+    const [isThemeBright, setIsThemeBright] = useState(false);
+    const toggleThemeBrightness = () => setIsThemeBright(!isThemeBright);
+    const dismantleColor = () => {
+        return currentColor
+            .replace(
+                /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+                (_, r, g, b) => `#${r}${r}${g}${g}${b}${b}`
+            )
+            .substring(1)
+            .match(/.{2}/g)
+            .map((x) => parseInt(x, 16));
+    };
+    const colorpack = currentColor ? dismantleColor() : null;
+    const lightervalues = Object.values(colorpack).map((rgbvalues) => {
+        return rgbvalues + 20 > 255 ? rgbvalues : rgbvalues + 20;
+    });
+    const darkervalues = Object.values(colorpack).map((rgbvalues) => {
+        return rgbvalues - 20 < 0 ? rgbvalues : rgbvalues - 20;
+    });
+    const evendarkervalues = Object.values(colorpack).map((rgbvalues) => {
+        return rgbvalues - 40 < 0 ? rgbvalues : rgbvalues - 40;
+    });
     const ran255 = () => {
         return Math.floor(Math.random() * 255);
     };
@@ -28,18 +104,72 @@ const ColorCard = () => {
         );
         return setCurrentColor(randomColorHex);
     };
-
+    const lightcolor = rebuildColor(
+        lightervalues[0],
+        lightervalues[1],
+        lightervalues[2]
+    );
+    const darkcolor = rebuildColor(
+        darkervalues[0],
+        darkervalues[1],
+        darkervalues[2]
+    );
+    const darkercolor = rebuildColor(
+        evendarkervalues[0],
+        evendarkervalues[1],
+        evendarkervalues[2]
+    );
     return (
-        <ColorCardContainer onClick={() => generateRandomColor()}>
-            <ColorCube
-                currentColor={currentColor}
-                rebuildColor={rebuildColor}
-            />
+        <ColorCardContainer
+            backgroundcolor={isThemeBright ? "#eee" : "#111"}
+            bordercolor={isThemeBright ? "#eee" : "#eee"}
+        >
+            <ClapOn onClick={() => toggleThemeBrightness()}>
+                <ClapOnLabel color={isThemeBright ? "#111" : "#eee"}>
+                    {isThemeBright ? "Dark" : "Light"}
+                </ClapOnLabel>
+            </ClapOn>
+            <ColorCubeDisplay>
+                <ColorCube
+                    isThemeBright={isThemeBright}
+                    currentColor={currentColor}
+                    setCurrentColor={setCurrentColor}
+                    generateRandomColor={generateRandomColor}
+                    lightcolor={lightcolor}
+                    darkcolor={darkcolor}
+                    darkercolor={darkercolor}
+                />
+            </ColorCubeDisplay>
+            <ColorDetails>
+                <DetailsDuo>
+                    <DetailsLabel color={isThemeBright ? "#111" : "#eee"}>
+                        Light:
+                    </DetailsLabel>
+                    <DetailsInfo color={isThemeBright ? "#111" : "#eee"}>
+                        {lightcolor}
+                    </DetailsInfo>
+                </DetailsDuo>
+                <DetailsDuo>
+                    <DetailsLabel color={isThemeBright ? "#111" : "#eee"}>
+                        Main:
+                    </DetailsLabel>
+                    <DetailsInfo color={isThemeBright ? "#111" : "#eee"}>
+                        {currentColor}
+                    </DetailsInfo>
+                </DetailsDuo>
+                <DetailsDuo>
+                    <DetailsLabel color={isThemeBright ? "#111" : "#eee"}>
+                        Dark:
+                    </DetailsLabel>
+                    <DetailsInfo color={isThemeBright ? "#111" : "#eee"}>
+                        {darkcolor}
+                    </DetailsInfo>
+                </DetailsDuo>
+            </ColorDetails>
         </ColorCardContainer>
     );
 };
 export default ColorCard;
-
 
 // make a shadow or glow option, whichever is chosen will change background container's color.
 // glow is on dark using the color already provided
--- src/components/colorcube/colorcube.component.jsx
+++ src/components/colorcube/colorcube.component.jsx
@@ -2,18 +2,21 @@ import styled from "styled-components";
 const ColorCubeCard = styled.div`
     width: 100px;
     height: 90px;
+    cursor: pointer;
     display: flex;
     align-items: center;
     justify-content: center;
-    cursor: pointer;
 `;
-const CubeHaus = styled.div`
+const CubeHaus = styled.div.attrs((props) => ({
+    style: {
+        transform: props.rotation,
+    },
+}))`
     width: 60px;
     height: 60px;
     top: 50%;
     left: 50%;
     transform-style: preserve-3d;
-    transform: ${({ rotation }) => rotation};
 `;
 const CubeSide = styled.div`
     position: absolute;
@@ -24,68 +27,63 @@ const CubeSide = styled.div`
     transition: 111ms linear;
     user-select: none;
 `;
-const CubeTop = styled(CubeSide)`
+const CubeTop = styled(CubeSide).attrs((props) => ({
+    style: {
+        backgroundColor: props.colorful,
+    },
+}))`
     transform: translateZ(-60px);
-    background: ${({ colorful }) => colorful};
 `;
 const CubeBottom = styled(CubeSide)`
     transform: translateZ(0px);
     background: ${({ colorful }) => colorful};
 `;
-const CubeRight = styled(CubeSide)`
+const CubeRight = styled(CubeSide).attrs((props) => ({
+    style: {
+        backgroundColor: props.colorful,
+    },
+}))`
     transform: rotateX(90deg);
     transform-origin: bottom;
-    background: ${({ colorful }) => colorful};
 `;
-const CubeLeft = styled(CubeSide)`
+const CubeLeft = styled(CubeSide).attrs((props) => ({
+    style: {
+        backgroundColor: props.colorful,
+    },
+}))`
     transform: rotateX(90deg) translateZ(60px);
     transform-origin: bottom;
-    background: ${({ colorful }) => colorful};
 `;
-const CubeBack = styled(CubeSide)`
+const CubeBack = styled(CubeSide).attrs((props) => ({
+    style: {
+        backgroundColor: props.colorful,
+    },
+}))`
     width: 60px;
     transform: translateZ(-60px) rotateY(90deg);
     transform-origin: right;
-    background: ${({ colorful }) => colorful};
 `;
-const CubeFront = styled(CubeSide)`
+const CubeFront = styled(CubeSide).attrs((props) => ({
+    style: {
+        backgroundColor: props.colorful,
+    },
+}))`
     width: 60px;
     transform: translateZ(-60px) rotateY(90deg) translateZ(-60px);
     transform-origin: right;
-    background: ${({ colorful }) => colorful};
 `;
 
-const ColorCube = ({ currentColor, rotation, rebuildColor }) => {
-    const dismantleColor = () => {
-        return currentColor
-            .replace(
-                /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
-                (_, r, g, b) => `#${r}${r}${g}${g}${b}${b}`
-            )
-            .substring(1)
-            .match(/.{2}/g)
-            .map((x) => parseInt(x, 16));
-    };
-    const colorpack = currentColor ? dismantleColor() : null;
-    const lightervalues = Object.values(colorpack).map((rgbvalues) => {
-        return rgbvalues + 20 > 255 ? rgbvalues : rgbvalues + 20;
-    });
-    const darkervalues = Object.values(colorpack).map((rgbvalues) => {
-        return rgbvalues - 20 < 0 ? rgbvalues : rgbvalues - 20;
-    });
-
-    const lightcolor = rebuildColor(
-        lightervalues[0],
-        lightervalues[1],
-        lightervalues[2]
-    );
-    const darkcolor = rebuildColor(
-        darkervalues[0],
-        darkervalues[1],
-        darkervalues[2]
-    );
+const ColorCube = ({
+    rotation,
+    isThemeBright,
+    currentColor,
+    generateRandomColor,
+    lightcolor,
+    darkcolor,
+    darkercolor,
+}) => {
     return (
-        <ColorCubeCard>
+        <ColorCubeCard onClick={() => generateRandomColor()}>
             <CubeHaus
                 rotation={
                     rotation
@@ -95,19 +93,18 @@ const ColorCube = ({ currentColor, rotation, rebuildColor }) => {
             >
                 <CubeTop colorful={currentColor ? lightcolor : "#fff"} />
                 <CubeFront
-                    colorful={currentColor ? currentColor : "transparent"}
+                    colorful={currentColor ? darkercolor : "transparent"}
                 />
                 <CubeBack colorful={currentColor ? currentColor : "#fefefe"} />
-                <CubeRight colorful={currentColor ? darkcolor : "#eee"} />
+                <CubeRight colorful={currentColor ? "transparent" : "#eee"} />
                 <CubeLeft
                     colorful={currentColor ? currentColor : "transparent"}
                 />
                 <CubeBottom
-                    colorful={currentColor ? currentColor : "transparent"}
+                    colorful={currentColor ? darkcolor : "transparent"}
                 />
             </CubeHaus>
         </ColorCubeCard>
     );
 };
 export default ColorCube;
-
