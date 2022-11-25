fc09248f3d20d9ba7f7fefbcb1ee31377f8368f9
Dylan <schiff.dylan@gmail.com>
2022-11-24 18:46:34 -0700

    404

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Home from "./components/pages/Home.component";
 import NotesHaus from "./components/notes/NoteHaus.component";
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<NavigationWrap />}>
-                <Route path="/" index element={<Home />}></Route>
-                <Route path="zz" element={<NotesHaus />}></Route>
-            </Route>
-        </Routes>
+        // <Routes>
+        //     <Route path="/" element={<NavigationWrap />}>
+        //         <Route path="/" index element={<Home />}></Route>
+        //         <Route path="zz" element={<NotesHaus />}></Route>
+        //     </Route>
+        // </Routes>
+        404
     );
 };
 export default App;
