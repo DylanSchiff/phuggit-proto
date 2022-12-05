1c1dbfcfd8c004ee37ada8c1420ff9505645b9df
Dylan <schiff.dylan@gmail.com>
2022-12-05 15:52:21 -0700

    login

--- src/AAAAAA.component.jsx
+++ src/AAAAAA.component.jsx
@@ -4,3 +4,15 @@ const AAAAAAA = () => {
     return <AAAAAAAContainer>AAAAAAA</AAAAAAAContainer>;
 };
 export default AAAAAAA;
+
+
+// import styled from "styled-components";
+// const AAAAAAAContainer = styled.div.attrs((props) => ({
+//     style: {
+//         color: props.AAAAAAAPropName,
+//     },
+// }))``;
+// const AAAAAAA = () => {
+//     return <AAAAAAAContainer>AAAAAAA</AAAAAAAContainer>;
+// };
+// export default AAAAAAA;
---
+++ src/components/resume/resume.component.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+const ResumeContainer = styled.div``;
+const Resume = () => {
+    return <ResumeContainer>Resume</ResumeContainer>;
+};
+export default Resume;
