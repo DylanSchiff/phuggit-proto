052ec0382adf3645c99fb5d1f60c917531c399fb
Dylan <schiff.dylan@gmail.com>
2022-12-05 16:20:06 -0700

    ok

--- src/ap/context/user.context.jsx
+++ src/ap/context/user.context.jsx
@@ -32,6 +32,7 @@ export const UserProvider = ({ children }) => {
                 createUserDocumentFromAuth(user);
             }
             setCurrentUser(user);
+            console.log(user)
         });
 
         return unsubscribe;
