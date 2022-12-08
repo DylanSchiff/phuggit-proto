0f7a697df9b9e671bba674b54a067b6cb6cc9b1c
Dylan <schiff.dylan@gmail.com>
2022-12-07 17:32:12 -0700

    logo

--- src/components/AAA-components/app-wrap/topbar-nameplate.component.jsx
+++ src/components/AAA-components/app-wrap/topbar-nameplate.component.jsx
@@ -1,10 +1,20 @@
 import styled from "styled-components";
-const TopbarNameplateContainer = styled.div`
+import logo from "../../../phuggitp.png";
+import { Link } from "react-router-dom";
+const TopbarNameplateContainer = styled(Link)`
     display: flex;
     align-items: center;
     justify-content: center;
 `;
+const TopbarNameplageImage = styled.img`
+    height: 50px;
+    object-fit: cover;
+`;
 const TopbarNameplate = () => {
-    return <TopbarNameplateContainer>Logo</TopbarNameplateContainer>;
+    return (
+        <TopbarNameplateContainer to="/">
+            <TopbarNameplageImage src={logo} />
+        </TopbarNameplateContainer>
+    );
 };
 export default TopbarNameplate;
